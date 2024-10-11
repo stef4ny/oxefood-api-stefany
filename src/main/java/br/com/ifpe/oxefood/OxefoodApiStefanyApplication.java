7f8bcae5d7896577a47c3836f8502df9650fb51a
Your Name <you@example.com>
2024-10-10 21:31:55 -0300

    primeiro arquivo

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiStefanyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiStefanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiStefanyApplication.class, args);
+	}
+
+}
