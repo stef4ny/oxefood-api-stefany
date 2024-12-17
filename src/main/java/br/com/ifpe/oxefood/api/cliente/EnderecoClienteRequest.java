98b0fd63f539308f3dfe335bb35321220fb4a1d4
Bowie <marketing@lestcode.com.br>
2024-12-16 23:18:11 -0300

    enderecoCliente

--- src/main/java/br/com/ifpe/oxefood/api/cliente/EnderecoClienteRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/EnderecoClienteRequest.java
@@ -1,9 +1,6 @@
 package br.com.ifpe.oxefood.api.cliente;
 
 import br.com.ifpe.oxefood.modelo.cliente.EnderecoCliente;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
